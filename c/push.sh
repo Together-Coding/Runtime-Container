79e142a61a1abd776f8516d36023839bdd4a1e61
a2tt <usera2tt@gmail.com>
2022-06-16 22:28:40 +0900

    feat: update runtime-agent

--- c/push.sh
+++ c/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build --no-cache -t toco-runtime-c --build-arg SSH_PRIVATE_KEY="$(cat ../toco)" .
+docker build -t toco-runtime-c --build-arg SSH_PRIVATE_KEY="$(cat ../toco)" .
 docker tag toco-runtime-c:latest 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com/toco-runtime-c:latest
 docker push 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com/toco-runtime-c:latest
--- python/push.sh
+++ python/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build --no-cache -t toco-runtime-py --build-arg SSH_PRIVATE_KEY="$(cat ../toco)" .
+docker build -t toco-runtime-py --build-arg SSH_PRIVATE_KEY="$(cat ../toco)" .
 docker tag toco-runtime-py:latest 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com/toco-runtime-py:latest
 docker push 093466323952.dkr.ecr.ap-northeast-2.amazonaws.com/toco-runtime-py:latest
